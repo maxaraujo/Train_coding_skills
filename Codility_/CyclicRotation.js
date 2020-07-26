20c966c296f57a558cc704476235c1e72be865c4
maxaraujo <maximillian.alex@gmail.com>
2020-07-25 21:11:38 -0300

    Rotate an array to the right by a given K of steps

---
+++ Codility_/CyclicRotation.js
@@ -0,0 +1,17 @@
+// by: Maximilliam Araujo
+// level:	Easy
+// Problem: 1 - CyclicRotation
+// Description: Rotate an array to the right by a given number of steps
+// Task Score:  100%
+// Correctness: 100%
+// Performance: Not assessed
+function CyclicRotation(A, K){
+  const aSize = A.length;
+  const rotation = K > aSize ? K % aSize : K;
+  if(aSize === rotation || rotation === 0)
+    return A;
+  
+  const rotatedNumbers = A.splice(aSize - rotation);
+  const result = rotatedNumbers.concat(A);
+  return result;
+}
